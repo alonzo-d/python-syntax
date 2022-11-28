75edd2bf03013891af69ff750ba82bb9123a9074
David A. Salazar <davidalonzso@gmail.com>
2022-11-28 13:16:30 -0800

    add words.py

--- words.py
+++ words.py
@@ -1,6 +1,17 @@
-def separate_words(words):
+# def print_upper_words(words):
+#     """ convert each word to uppercase then print separately """
+#     for word in words:
+#         print(word.upper())
+
+
+# print_upper_words(['boat', 'car', 'train', 'plane'])
+
+def print_only_es(words):
+    """ print only words that begin with 'e' """
+
     for word in words:
-        print(word.upper())
+        if word.startswith('e'):
+            print(word)
 
 
-separate_words(['boat', 'car', 'train', 'plane'])
+print_only_es(['apple', 'orange', 'elote'])
